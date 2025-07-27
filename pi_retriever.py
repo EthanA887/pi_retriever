a7f5ac45e84bb98a3772dd55082a8aa9d5b59cd2
Ethan <70213195+EthanA887@users.noreply.github.com>
2025-07-27 13:11:46 -0400

    Update pi_retriever.py

--- pi_retriever.py
+++ pi_retriever.py
@@ -14,20 +14,26 @@ with open('performance_indicators.txt', 'w') as pi_list:
             strikes = 0
             time = 0
             for line in text:
+                """
+                Start detecting only when the following phrases are seen:
+                1. SPECIFIC COMPETENCIES EVALUATED
+                2. SPECIFIC PERFORMANCE INDICATORS
+                3. PERFORMANCE INDICATORS
+                """
                 if ('PERFORMANCE INDICATORS' in line and 'GENERAL' not in line) or ('SPECIFIC' in line):
-                    begin_detection = True
+                    begin_detection = True # List of PIs found
                 elif begin_detection:
                     if line.strip() == '':
                         strikes += 1
-                    if strikes == 6:
+                    if strikes == 5: # Max. 5 lines before the next section
                         break
                     while True:
-                        try:
+                        try: # Attempt to parse line (the program throws an error if it detects unknown characters e.g. bullet points)
                             if line != '':
                                 if line[0].isnumeric():
                                     line = line[3:]
                                 pi_list.write(line.strip() + '\n')
-                        except:
+                        except: # Remove first character
                             line = line[1:]
                         else:
                             break
@@ -35,7 +41,7 @@ with open('performance_indicators.txt', 'w') as pi_list:
             if file not in ['pi_retriever.py', 'performance_indicators.txt']:
                 print('Error: file could not be read. Proceeding to next file.')
 
-# Normalize data
+# Remove blank lines
 with open('performance_indicators.txt', 'r') as file:
     lines = file.readlines()
 
@@ -52,8 +58,6 @@ with open('performance_indicators.txt', 'r') as file:
     x = int(input('Number of most common PIs to display: '))
     popular = Counter(pis).most_common(x)
     print(f'Top {x} most common PIs:')
-    for i, count in popular:
+    for i, count in popular: # Enumerate over sorted results by frequency
         print(f'{i[:-2]}: {count} occurrences')
     print('----------------')
-
-# Created by Ethan Ali
